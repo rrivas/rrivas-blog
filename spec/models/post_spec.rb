c95762da68a033362c55cc02e483a54789256a03
Robert Rivas <robertrivas@gmail.com>
2012-07-02 23:29:54 -0700

    Added initial test for posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'title' do
+    it 'should not allow multiple posts with the same title'
+    it 'should require a title'
+    it 'should require titles with at least 5 characters'
+  end
 end
