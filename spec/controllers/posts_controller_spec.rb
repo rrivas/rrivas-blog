0fb6454e5fccece96f3786fafeaba311b4613082
Robert Rivas <robertrivas@gmail.com>
2012-07-07 15:58:44 -0700

    Added testing for post show and index

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,6 +2,18 @@ require 'spec_helper'
 
 describe PostsController do
   describe "Get Index" do
-    it "assigns all posts to @posts"
+    it "assigns all posts to @posts" do
+      post = create(:post)
+      get :index
+      assigns( :posts ).should eq([post])
+    end
+  end
+
+  describe "Show Post" do
+    it "should assign the correct post to @post" do
+      post = create(:post)
+      get :show, id: post.id
+      assigns( :post ).should eq(post)
+    end
   end
 end
