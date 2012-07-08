291dc2ffc71fb8490c42c239619cbe64978cadde
Robert Rivas <robertrivas@gmail.com>
2012-07-08 00:02:52 -0700

    Added length validation for first name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name
 
   validates :first_name, presence: true,
-                         format: { with: /([a-z][A-Z]){1,}/, on: :create }
+                         format: { with: /([a-z][A-Z]){1,}/, on: :create },
+                         length: { minimum: 2 }
 
   validates :last_name, presence: true,
                         format: { with: /([a-z][A-Z]){1,}/, on: :create },
