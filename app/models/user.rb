94a115d3e9a092bd6593556bc221999519cda3d5
Robert Rivas <robertrivas@gmail.com>
2012-07-11 01:05:53 -0700

    Added has_secured_password method to storing passwords for the users. Removed encrypt_password method along with hash and salt columns. Added digest migration to accomodate has_secured_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation
+  has_secure_password
 
-  attr_accessor :password
-  before_save :encrypt_password
+  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation
 
   has_many :posts
 
@@ -17,11 +16,4 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: { case_sensitive: false },
                     presence: true,
                     format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
-
-  def encrypt_password
-    if password.present?
-      self.password_salt = BCrypt::Engine.generate_salt
-      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
-    end
-  end
 end
