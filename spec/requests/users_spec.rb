7971a5e461b23b0ea03c8864f1ef7100bf71eeab
Robert Rivas <robertrivas@gmail.com>
2012-07-15 22:22:48 -0700

    Added test for checking user/new

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,63 @@
+require 'spec_helper'
+
+describe 'Users' do
+  context 'New User' do
+    before(:each) do
+      visit new_user_path
+    end
+
+    it "should have a page title of 'New User'" do
+      page.should have_css('.page-header h1.title', text: 'New User')
+    end
+
+    it "should have a submit button"
+
+    describe 'first name' do
+      it "should have a field" do
+        page.has_field?('First name').should be_true
+      end
+    end
+
+    describe 'last name' do
+      it "should have a field" do
+        page.has_field?('Last name').should be_true
+      end
+    end
+
+    describe 'eamil address' do
+      it 'should have a field' do
+        page.has_field?('Email').should be_true
+      end
+    end
+
+    describe 'password' do
+      it 'should have a field' do
+        page.has_field?('Password').should be_true
+      end
+
+      it 'should be required' do
+        page.find_field('Password')[:class].split(' ').include?('required').should be_true
+      end
+    end
+
+    describe 'password confirmation' do
+      it 'should have a field' do
+        page.has_field?('Password confirmation').should be_true
+      end
+
+      it 'should be required' do
+        page.find_field('Password confirmation')[:class].split(' ').include?('required').should be_true
+      end
+    end
+
+    describe 'active' do
+      it 'should have a field' do
+        page.has_field?('Active').should be_true
+      end
+
+      it 'should be checked by default' do
+        page.find_field('Active').checked?.should be_true
+      end
+    end
+  end
+end
