04de32fe1d0a01ee8edc57f46e6c1f7f0759805b
Robert Rivas <robertrivas@gmail.com>
2012-07-15 23:50:32 -0700

    user that doesn't pass validation renders :new.  Tests included

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new( params[:user] )
+
+    if @user.save
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,4 +7,27 @@ describe UsersController do
       assigns( :user ).attributes.should eq( User.new.attributes )
     end
   end
+
+  context 'Post create' do
+    before(:each) do
+      @user = build( :user )
+      @user_attributes = { email: @user.email, 
+                           first_name: @user.first_name, 
+                           last_name: @user.last_name,
+                           password: @user.password,
+                           password_confirmation: @user.password,
+                           active: @user.active }
+    end
+
+    it 'builds a user and saves it' do
+      post :create, user: @user_attributes
+      assigns( :user ).new_record?.should be_false
+    end
+
+    it 'redirects to user show page on succesful save'
+    it 'redirects to user new page on unsuccesful save' do
+      post :create, user: @user_attributes.merge( password: nil )
+      response.should render_template( :new )
+    end
+  end
 end
